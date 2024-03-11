359f6a052517f9acc5f071850fc3f3b5ae70f271
Ravshan007 <ravshanaliyev9002@gmail.com>
2024-03-11 16:46:49 +0500

    skeleton added

--- src/components/shared/banner.tsx
+++ src/components/shared/banner.tsx
@@ -7,23 +7,37 @@ import {
     CarouselPrevious,
 } from "@/components/ui/carousel"
 const Banner = () => {
-    const { data } = useGetBanner()
+    const { data, isLoading } = useGetBanner()
 
     return (
         <div>
-            <Carousel className="w-full ">
-                <CarouselContent>
-                    {
-                        data?.map((item: any) => (
-                            <CarouselItem key={item?.id}>
-                                <img className="h-[120px] md:h-auto" src={item?.img} alt="" />
-                            </CarouselItem>
-                        ))
-                    }
-                </CarouselContent>
-                <CarouselPrevious className="w-10 h-10 left-6" />
-                <CarouselNext className="w-10 h-10 right-8" />
-            </Carousel>
+            {
+                isLoading ? (
+                    <div>
+                        <div role="status" className="flex items-center justify-center h-[316px] max-w-full bg-gray-300 rounded-lg animate-pulse dark:bg-gray-700">
+                            <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                                <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                                <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM9 13a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2v-2a2 2 0 0 1 2-2h2a2 2 0 0 1 2 2v2Zm4 .382a1 1 0 0 1-1.447.894L10 13v-2l1.553-1.276a1 1 0 0 1 1.447.894v2.764Z" />
+                            </svg>
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                    </div>
+                ) : (
+                    <Carousel className="w-full ">
+                        <CarouselContent>
+                            {
+                                data?.map((item: any) => (
+                                    <CarouselItem key={item?.id}>
+                                        <img className="h-[120px] md:h-auto" src={item?.img} alt="" />
+                                    </CarouselItem>
+                                ))
+                            }
+                        </CarouselContent>
+                        <CarouselPrevious className="w-10 h-10 left-6" />
+                        <CarouselNext className="w-10 h-10 right-8" />
+                    </Carousel>
+                )
+            }
         </div>
     )
 }
--- src/components/shared/products.tsx
+++ src/components/shared/products.tsx
@@ -14,38 +14,135 @@ import { addToCart } from "@/redux/slices/cart-slice"
 import { Link } from "react-router-dom"
 const Products = () => {
     const dispatch = useDispatch()
-    const { data } = useGetProducts("all")
+    const { data, isLoading } = useGetProducts("all")
     const handleAddToCart = (data: any) => {
         dispatch(addToCart(data))
     }
+    console.log(isLoading);
+
     return (
-        <div className="w-[1440px] mx-auto my-6">
-            <h1 className="text-2xl my-4 px-4">Смартфоны и планшеты</h1>
-            <Carousel className="w-[390px] md:w-[768px] lg:w-full ">
-                <CarouselContent>
-                    {data?.map((item: any, index: number) => (
-                        <CarouselItem key={index} className="basis-1/2 md:basis-1/4 lg:basis-1/6">
-                            <div className="p-1 h-[350px] ">
-                                <Card>
-                                    <CardContent className="flex aspect-square items-center justify-center p-3 flex-col">
-                                        <Link to={`/product/${item?.id}`}>
-                                            <img className="w-[100px] md:w-[150px] lg:h-[200px] lg:w-[200px]" src={item?.img} alt="" />
-                                            <h3 className="text-[18px] mt-2">{item.title.length > 40 ? item.title.slice(0, 40) + "..." : item.title}</h3>
-                                        </Link>
-                                        <div className="w-full flex justify-between items-center mt-2">
-                                            <p className="text-[18px]">{item?.price} сум</p>
-                                            <Button onClick={() => handleAddToCart(item)} className="bg-[#FEEE00]  hover:bg-[#fff45a] text-[#000000]"><ShoppingCart /></Button>
-                                        </div>
-                                    </CardContent>
-                                </Card>
+        <>
+            {
+                isLoading ? (
+                    <div className="w-[1440px] mx-auto my-6 h-[400px] flex gap-4 justify-between">
+                        <div role="status" className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
+                            <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+                                <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                                    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                                </svg>
                             </div>
-                        </CarouselItem>
-                    ))}
-                </CarouselContent>
-                <CarouselPrevious className="w-10 h-10 left-[0px] lg:left-[-20px]" />
-                <CarouselNext className="w-10 h-10 right-[0px] lg:right-[-20px]" />
-            </Carousel>
-        </div>
+                            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                            <div className="flex items-center mt-4">
+                                <svg className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                                </svg>
+                                <div>
+                                    <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
+                                    <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                                </div>
+                            </div>
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                        <div role="status" className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
+                            <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+                                <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                                    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                                </svg>
+                            </div>
+                            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                            <div className="flex items-center mt-4">
+                                <svg className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                                </svg>
+                                <div>
+                                    <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
+                                    <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                                </div>
+                            </div>
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                        <div role="status" className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
+                            <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+                                <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                                    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                                </svg>
+                            </div>
+                            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                            <div className="flex items-center mt-4">
+                                <svg className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                                </svg>
+                                <div>
+                                    <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
+                                    <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                                </div>
+                            </div>
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                        <div role="status" className="max-w-sm p-4 border border-gray-200 rounded shadow animate-pulse md:p-6 dark:border-gray-700">
+                            <div className="flex items-center justify-center h-48 mb-4 bg-gray-300 rounded dark:bg-gray-700">
+                                <svg className="w-10 h-10 text-gray-200 dark:text-gray-600" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 16 20">
+                                    <path d="M14.066 0H7v5a2 2 0 0 1-2 2H0v11a1.97 1.97 0 0 0 1.934 2h12.132A1.97 1.97 0 0 0 16 18V2a1.97 1.97 0 0 0-1.934-2ZM10.5 6a1.5 1.5 0 1 1 0 2.999A1.5 1.5 0 0 1 10.5 6Zm2.221 10.515a1 1 0 0 1-.858.485h-8a1 1 0 0 1-.9-1.43L5.6 10.039a.978.978 0 0 1 .936-.57 1 1 0 0 1 .9.632l1.181 2.981.541-1a.945.945 0 0 1 .883-.522 1 1 0 0 1 .879.529l1.832 3.438a1 1 0 0 1-.031.988Z" />
+                                    <path d="M5 5V.13a2.96 2.96 0 0 0-1.293.749L.879 3.707A2.98 2.98 0 0 0 .13 5H5Z" />
+                                </svg>
+                            </div>
+                            <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-48 mb-4"></div>
+                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700 mb-2.5"></div>
+                            <div className="h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                            <div className="flex items-center mt-4">
+                                <svg className="w-10 h-10 me-3 text-gray-200 dark:text-gray-700" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
+                                    <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z" />
+                                </svg>
+                                <div>
+                                    <div className="h-2.5 bg-gray-200 rounded-full dark:bg-gray-700 w-32 mb-2"></div>
+                                    <div className="w-48 h-2 bg-gray-200 rounded-full dark:bg-gray-700"></div>
+                                </div>
+                            </div>
+                            <span className="sr-only">Loading...</span>
+                        </div>
+                    </div>
+                ) : <div className="w-[1440px] mx-auto my-6">
+                    <h1 className="text-2xl my-4 px-4">Смартфоны и планшеты</h1>
+                    <Carousel className="w-[390px] md:w-[768px] lg:w-full ">
+                        <CarouselContent>
+                            {data?.map((item: any, index: number) => (
+                                <CarouselItem key={index} className="basis-1/2 md:basis-1/4 lg:basis-1/6">
+                                    <div className="p-1 h-[350px] ">
+                                        <Card>
+                                            <CardContent className="flex aspect-square items-center justify-center p-3 flex-col">
+                                                <Link to={`/product/${item?.id}`}>
+                                                    <img className="w-[100px] md:w-[150px] lg:h-[200px] lg:w-[200px]" src={item?.img} alt="" />
+                                                    <h3 className="text-[18px] mt-2">{item.title.length > 40 ? item.title.slice(0, 40) + "..." : item.title}</h3>
+                                                </Link>
+                                                <div className="w-full flex justify-between items-center mt-2">
+                                                    <p className="text-[18px]">{item?.price} сум</p>
+                                                    <Button onClick={() => handleAddToCart(item)} className="bg-[#FEEE00]  hover:bg-[#fff45a] text-[#000000]"><ShoppingCart /></Button>
+                                                </div>
+                                            </CardContent>
+                                        </Card>
+                                    </div>
+                                </CarouselItem>
+                            ))}
+                        </CarouselContent>
+                        <CarouselPrevious className="w-10 h-10 left-[0px] lg:left-[-20px]" />
+                        <CarouselNext className="w-10 h-10 right-[0px] lg:right-[-20px]" />
+                    </Carousel>
+                </div>
+            }
+        </>
     )
 }
 
